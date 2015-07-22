e8e4bfe7376fa5facd1a11027ddea1d31657feba
Dan <dchen12812@gmail.com>
2015-07-21 23:06:40 -0700

    77% accuacy with class and gender

--- Titanic Machine Learning from Disaster/titantic_pred.R
+++ Titanic Machine Learning from Disaster/titantic_pred.R
@@ -8,12 +8,24 @@ test = read.csv("test.csv", stringsAsFactors=FALSE)
 library(ROCR)
 library(randomForest)
 
-#RF on class and prediction
-classRF = randomForest(as.factor(Survived) ~ Pclass, data=train)
+#RF on class and prediction => .65% accuracy
+#classRF = randomForest(as.factor(Survived) ~ Pclass, data=train)
+#PredTest = predict(classRF, newdata=test)
+
+#Convert male = 0, female = 1 for sex
+train$Sex[which(train$Sex == 'female')] = 1
+train$Sex[which(train$Sex == 'male')] = 0
+test$Sex[which(test$Sex == 'female')] = 1
+test$Sex[which(test$Sex == 'male')] = 0
+
+
+#RF on class and sex and prediction => 77% accuracy
+classRF = randomForest(as.factor(Survived) ~ Pclass + Sex, data=train)
 PredTest = predict(classRF, newdata=test)
 
+
 #Preps file for kaggle submission
-MySubmission = data.frame(PassengerID = test$PassengerID, Survived = PredTest)
+MySubmission = data.frame(PassengerID = test$PassengerId, Survived = PredTest)
 
 #Creates csv for kaggle
 write.csv(MySubmission, "prediction.csv", row.names=FALSE)
