ec0e5a44fd6199a08aec0702de7eff957d0a77df
Dan <dchen12812@gmail.com>
2015-09-03 16:04:15 -0700

    init project

---
+++ San Francisco Crime Classification/crime_classifier.R
@@ -0,0 +1,22 @@
+#San Francisco Crime Classification
+
+#Loads libraries
+library(ggmap)
+library(ggplot2)
+library(dplyr)
+
+#Initializes data
+train = read.csv("Data/train.csv")
+map = get_map(location="sanfrancisco",zoom=12,source="osm")
+
+#Function to plot graph of locations of crime for visualization purposes
+map_crime = function(crime_df, crime) {
+    filtered <- filter(crime_df, Category %in% crime)
+    plot <- ggmap(map, extent='device') + 
+        geom_point(data = filtered, aes(x = X, y = Y, color=Category, alpha=0.6))
+    return(plot)
+}
+
+#Plots crimes
+map_crime(train, c('SUICIDE', 'ARSON'))
+#map_crime(train, unique(train$Category))
