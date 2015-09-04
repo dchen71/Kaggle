73cb8d88537c8dd2eadea591153157890f087a27
Dan <dchen12812@gmail.com>
2015-09-03 23:15:22 -0700

    forked script

--- San Francisco Crime Classification/crime_classifier.R
+++ San Francisco Crime Classification/crime_classifier.R
@@ -1,23 +1,52 @@
-#San Francisco Crime Classification
+library(MASS)
+library(readr)
+#library (rpart)
+library(caret)
+library(lubridate)
 
-#Loads libraries
 
+treatment<-function(fname){
+    df<-read.csv(paste0('Data/',fname))
+    Dates1  = strptime(as.character(df$Dates),"%Y-%m-%d %H:%M:%S")
+    print(str(Dates1))
+    df$Year = Dates1$year
+    df$Month = Dates1$mon
+    df$Hour = as.numeric(format(ymd_hms(Dates1), "%H"))
+    df$Loc = as.factor(paste(round(df$X,2), round(df$Y,2), sep= " "))
+    df$XY <- -df$X * df$Y    
+    df$XandY <- -df$X + df$Y
+    df$AddOf <- sapply(df$Address, FUN=function(x) {strsplit(as.character(x), split="of ")[[1]][2]})
+    df$AddType <- as.factor(ifelse(is.na(df$AddOf ),1,2))
+    return(df)
+}
+train<-treatment('train.csv')
+print(str(train))
+print('Read train data complete...')
+test<-treatment('test.csv')
+print('Read test data complete...')
 
-#Initializes data
-train = read.csv("Data/train.csv")
-test = read.csv('Data/test.csv')
+##split train data into 10 paritions due to memory space constraint
+inTrain<-createDataPartition(train$Category,p=0.55,list=F)
+train.sub<-train[inTrain,]
 
-categories = data.frame(cat = unique(levels(train$Category)))
-
-#Trying glm prediction
-log_model1 = glm(Category ~ PdDistrict + X + Y, data=train, family=binomial)
-pred_test1 = predict(log_model1,newdata=test ,type="response")
-
-#Shaping data for saving
-save_data = data.frame(Id = test$Id)
-save_data[,2:(length(unique(train$Category)) + 1)] = 0
-names(save_data) = c('Id', unique(levels(train$Category)))
-save_data[,2] = pred_test1
-
-#Save the data to a csv
-write.csv(save_data, file="classification.csv", row.names=FALSE)
+rpart.train<-function(train,test){
+    submission<-data.frame(Id=test$Id)
+    response<-data.frame(Cat=train$Category)
+    
+    crime<-as.character(unique(train$Category))
+    crime<-sort(crime)
+    for (i in crime){
+        response[i]<- 0
+        response[i][response$Cat==i,]<- 1
+        fit<-glm(response[,i]~PdDistrict+X+Y+XY+XandY+AddType+DayOfWeek+Year+Hour+Month ,data=train, family = binomial)
+        pred <- predict(fit,test, type = "response")
+        submission[i]<-pred
+        print(paste0(ncol(submission)/length(crime)*100,'% completed'))
+    }
+    return(submission)
+}
+submission<-rpart.train(train.sub,test)
+rm(train, test,train.sub)
+gz_out <- gzfile("submit.csv.gz", "w")
+writeChar(write_csv(submission, ""), gz_out, eos=NULL)
+close(gz_out)
