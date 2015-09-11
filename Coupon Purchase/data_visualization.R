2f4594747f78ef74b080e2bff5b05b496963bc99
Dan <dchen12812@gmail.com>
2015-09-11 16:01:12 -0700

    updated with locales to fix display

--- Coupon Purchase/data_visualization.R
+++ Coupon Purchase/data_visualization.R
@@ -2,36 +2,32 @@
 
 #Loads libraries
 library(ggplot2)
-library(extrafont)
 
 #Read input
-area_train      <<- read.csv('input/coupon_area_train.csv') #coupon per area
-detail_train    <<- read.csv('input/coupon_detail_train.csv') #details on bought or not
-list_train      <<- read.csv('input/coupon_list_train.csv') #coupon details
-visit_train     <<- read.csv('input/coupon_visit_train.csv') #referrers and bought or not
-user_list       <<- read.csv('input/user_list.csv') #user details
+area_train      <<- read.csv('input/coupon_area_train.csv', encoding="UTF-8") #coupon per area
+detail_train    <<- read.csv('input/coupon_detail_train.csv', encoding="UTF-8") #details on bought or not
+list_train      <<- read.csv('input/coupon_list_train.csv', encoding="UTF-8") #coupon details
+user_list       <<- read.csv('input/user_list.csv', encoding="UTF-8") #user details
 
-#Install fonts
-font_import()
-fonts()
-fonttable()
+#Change locale for characters
+Sys.setlocale(category="LC_ALL", locale = "japanese")
 
 #Plot the number of coupons sold per prefecture
 pref_coupons = as.data.frame(table(area_train$PREF_NAME))
 names(pref_coupons) = c('pref','freq')
-ggplot(data=area_train,aes(x=as.factor(PREF_NAME))) + geom_bar(stat='bin') + 
+ggplot(data=area_train,aes(x=PREF_NAME)) + geom_bar(stat='bin') + 
     labs(title="Coupons bought per prefecture" ,x="Prefecture", y="Coupons bought") + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 1,family="MS Mincho"))
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 #Plot the number of coupons sold per prefecture(smaller)
 small_pref_coupons = as.data.frame(table(area_train$SMALL_AREA_NAME))
 names(small_pref_coupons) = c('pref','freq')
 ggplot(data=area_train,aes(x=as.factor(SMALL_AREA_NAME))) + geom_bar(stat='bin') + 
     labs(title="Coupons bought per area" ,x="Area", y="Coupons bought") + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 1,family="MS Mincho"))
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 #Plot number of male to female
-mf <- ggplot(user_list, aes(x = factor(1), fill = factor(SEX_ID))) +
+mf = ggplot(user_list, aes(x = factor(1), fill = factor(SEX_ID))) +
     geom_bar(width = 1) + labs(title="Ratio of male to female users", x="",y="") +
     scale_fill_discrete(name="Sex")
 mf + coord_polar(theta = "y")
@@ -46,8 +42,11 @@ ages[3,2] = data.frame(table(user_list$AGE > 25,user_list$AGE <= 35))[4,3]
 ages[4,2] = data.frame(table(user_list$AGE > 35,user_list$AGE <= 50))[4,3]
 ages[5,2] = data.frame(table(user_list$AGE > 50,user_list$AGE <= 65))[4,3]
 ages[6,2] = data.frame(table(user_list$AGE > 65,user_list$AGE <= 80))[2,3]
-age <- ggplot(ages, aes(x = factor(1), y=Count,fill = factor(Group))) +
+age = ggplot(ages, aes(x = factor(1), y=Count,fill = factor(Group))) +
     geom_bar(width = 1,stat="identity") + labs(title="Age distribution of users", x="",y="") +
     scale_fill_discrete(name="Age groups")
 age + coord_polar(theta = "y")
 
+
+#Restore locale
+Sys.setlocale(category="LC_ALL", locale = "English_United States.1252")
