1920473a9d6019cefb38230c6a130d724e3ad66f
Dan <dchen12812@gmail.com>
2015-10-05 20:18:42 -0700

    setup lm

--- Store Sales/sales_predictor.R
+++ Store Sales/sales_predictor.R
@@ -9,6 +9,14 @@ test = read.csv(paste0(dir,"test.csv"))
 #Setup cross validation set
 library(caret)
 
+#Seed for consistent results
+set.seed(100)
+
 part = createDataPartition(train$Sales, p = 0.6, list = FALSE)
 trainset = train[part,]
 testset = train[-part,]
+
+#Setup Linear model
+lmTrain = lm(Sales ~ . - Date, trainset)
+predTrain = predict(lmTrain, trainset)
+predTest = predict(lmTrain, newdata=testset)
