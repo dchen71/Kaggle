155a7422332571d0c520c86085dbdac9eb9f0f0e
Dan <dchen12812@gmail.com>
2015-10-05 20:00:28 -0700

    init proj

---
+++ Store Sales/sales_predictor.R
@@ -0,0 +1,8 @@
+#Kaggle Competition - Rossmann Store Sales
+
+#Read input
+dir = 'input/'
+store = read.csv(paste0(dir,"store.csv"))
+train = read.csv(paste0(dir,"train.csv"))
+test = read.csv(paste0(dir,"test.csv"))
+
