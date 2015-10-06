32d9a4d5dcc711d1eb0460627c8fb2ccfa0ad38c
Dan <dchen12812@gmail.com>
2015-10-05 20:31:03 -0700

    code to write to csv

--- Store Sales/sales_predictor.R
+++ Store Sales/sales_predictor.R
@@ -16,7 +16,17 @@ part = createDataPartition(train$Sales, p = 0.6, list = FALSE)
 trainset = train[part,]
 testset = train[-part,]
 
-#Setup Linear model
+#Setup Linear model for CV
 lmTrain = lm(Sales ~ . - Date, trainset)
 predTrain = predict(lmTrain, trainset)
 predTest = predict(lmTrain, newdata=testset)
+
+#Create a linear model
+lmModel = lm(Sales ~ . - Date - Customers, train)
+predModel = predict(lmModel, newdata = test)
+
+#Creates submission
+submission = data.frame(test$Id)
+names(submission) = 'ID'
+submission$Sales = predModel
+write.csv(submission, file="sales.csv", row.names=FALSE)
