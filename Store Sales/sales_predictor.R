e6c8564aca6638854e523d18ac0674b1e056f1cd
Dan <dchen12812@gmail.com>
2015-10-06 16:11:10 -0700

    merge and break apart train and test data

--- Store Sales/sales_predictor.R
+++ Store Sales/sales_predictor.R
@@ -6,36 +6,48 @@ store = read.csv(paste0(dir,"store.csv"))
 train = read.csv(paste0(dir,"train.csv"))
 test = read.csv(paste0(dir,"test.csv"))
 
-#Setup cross validation set
+#Data Cleanup
+##Fix NAs in Open in test
+test$Open[which(is.na(test$Open))] = 0
+
+id = test$Id
+
+##Remove Customers from train and date from both
+train = train[,c(-3,-5)]
+test = test[,c(-1,-4)]
+
+##Merge datasets together for feature engineering
+test$Sales = 'dummy'
+train = rbind(train, test)
+
+# Separate the test from train
+test = train[train$Sales=="dummy",]
+test$Sales = NULL
+train = train[train$Sales !="dummy",]
+
+#Testing Model
+##Setup cross validation set
 library(caret)
 
-#Seed for consistent results
+##Seed for consistent results
 set.seed(100)
 
 part = createDataPartition(train$Sales, p = 0.6, list = FALSE)
 trainset = train[part,]
 testset = train[-part,]
 
-#Fix NAs in Open in test
-test$Open[which(is.na(test$Open))] = 0
-
-#Setup Linear model for CV
-lmTrain = lm(Sales ~ . - Date, trainset)
+##Setup Linear model for CV
+lmTrain = lm(Sales ~ ., trainset)
 predTrain = predict(lmTrain, trainset)
 predTest = predict(lmTrain, newdata=testset)
 
-#Remove Customers from train and date from both
-train = train[,c(-3,-5)]
-test = test[,-4]
-
-#Create a linear model
+#Prediction
+##Create a linear model
 lmModel = lm(Sales ~ ., train)
 predModel = predict(lmModel, newdata = test)
 
-predModel[which(is.na(predModel))] = 0
-
 #Creates submission
-submission = data.frame(test$Id)
+submission = data.frame(id)
 names(submission) = 'ID'
 submission$Sales = predModel
 write.csv(submission, file="sales.csv", row.names=FALSE)
