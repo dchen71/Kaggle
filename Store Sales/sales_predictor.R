21c21ee8487a13712e4a79eddc873c2c5fe2f98c
Dan <dchen12812@gmail.com>
2015-10-05 20:03:07 -0700

    added cv

--- Store Sales/sales_predictor.R
+++ Store Sales/sales_predictor.R
@@ -6,3 +6,9 @@ store = read.csv(paste0(dir,"store.csv"))
 train = read.csv(paste0(dir,"train.csv"))
 test = read.csv(paste0(dir,"test.csv"))
 
+#Setup cross validation set
+library(caret)
+
+part = createDataPartition(train$Sales, p = 0.6, list = FALSE)
+trainset = train[part,]
+testset = train[-part,]
