32fe3e8d121f8c45a25f533893a54a83b94a843b
Dan <dchen12812@gmail.com>
2015-11-03 20:53:30 -0800

    function to preprocess data

--- What's Cooking/classifier.R
+++ What's Cooking/classifier.R
@@ -1,7 +1,29 @@
 #Kaggle Competition - What's Cooking
 #Predict category of cuisine based on ingredients
 
+#Init libraries
+library(jsonlite) #Read json
+library(tm) #Parse ingredients and get corpus
+
 #Read input
-library(jsonlite)
-test = fromJSON("test.json")
-train = fromJSON("train.json")
+test = fromJSON("test.json", flatten=TRUE)
+train = fromJSON("train.json", flatten=TRUE)
+
+#Preprocesses the data
+preProcess = function(df){
+    df$ingredients = lapply(df$ingredients, FUN=function(x) gsub("-", "_", x)) # Subs - with _
+    df$ingredients = lapply(df$ingredients, FUN=function(x) gsub("[^a-z0-9_ ]", "", x)) # Allow regular character and spaces    
+    return(df)
+}
+
+test = preProcess(test)
+train = preProcess(train)
+
+
+
+
+#Write to csv
+submission = data.frame(test$id)
+names(submission) = 'id'
+#submission$cusine = predModel
+write.csv(submission,filename="submission.csv",row.names=FALSE)
