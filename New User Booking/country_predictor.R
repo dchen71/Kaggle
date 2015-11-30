8eb6329a45d506b60f907fc8f9d8e353537a9281
Dan <dchen12812@gmail.com>
2015-11-30 15:57:08 -0800

    added features

--- New User Booking/country_predictor.R
+++ New User Booking/country_predictor.R
@@ -1,5 +1,7 @@
 #Kaggle Competition - New User Bookings
 
+library(lubridate)
+
 #Read input
 dir = 'input/'
 train = read.csv(paste0(dir,"train_users.csv"))
@@ -11,10 +13,16 @@ sessions  = read.csv(paste0(dir,"sessions.csv"))
 #Preprocess data
 ##age - Merge age and train/test based on age bucket(not sure for na)
 ##countries - use distance, destination lang, destination, may have issues with test set
+###Consider adding in countries via columns with desitation, distance, language, levenshtein
 
 #Preprocesses the data for both train and test
 preprocess = function(df){
     df = merge(df, sessions, by.x="id", by.y="user_id")
+    df$month_created = month(df$date_account_created)
+    df$year_created = year(df$date_account_created)
+    df$year_booked = year(df$date_first_booking)
+    df$month_booked = month(df$date_first_booking)
+    df$diff_booked = as.Date(df$date_first_booking) - as.Date(df$date_account_created)
     return(df)
 }
 
