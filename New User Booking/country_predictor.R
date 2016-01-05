c2487de2bf8353971efdced4aa1df225f539bd05
Dan <dchen12812@gmail.com>
2016-01-05 00:01:59 -0800

    removed loading of ages and chnaged preprocess for ages

--- New User Booking/country_predictor.R
+++ New User Booking/country_predictor.R
@@ -7,24 +7,45 @@ dir = 'input/'
 train = read.csv(paste0(dir,"train_users.csv"))
 test = read.csv(paste0(dir,"test_users.csv"))
 countries = read.csv(paste0(dir,"countries.csv"))
-age = read.csv(paste0(dir, "age_gender_bkts.csv"))
+
 sessions  = read.csv(paste0(dir,"sessions.csv"))
 
 #Preprocess data
-##age - Merge age and train/test based on age bucket(not sure for na)
 ##countries - use distance, destination lang, destination, may have issues with test set
 ###Consider adding in countries via columns with desitation, distance, language, levenshtein
 
+#convert dates to date format first
+
+#timestamp_firstactive seems be be setup yearmonthdayhourminutesecond
+##should sort before checking this
+##seems to be 20140101-20140331 for train
+##seems to be 20140401-20140630 for test
+
+#date_account_created and date_first_booking need to be converted into date
+##age seems to have na min age is 14 and max is 2014
+#table(train$age)
+
 #Preprocesses the data for both train and test
+#preprocess = function(df){
+#    df = merge(df, sessions, by.x="id", by.y="user_id")
+#    df$month_created = month(df$date_account_created)
+#    df$year_created = year(df$date_account_created)
+#    df$year_booked = year(df$date_first_booking)
+#    df$month_booked = month(df$date_first_booking)
+#    df$diff_booked = as.Date(df$date_first_booking) - as.Date(df$date_account_created)
+#    return(df)
+#}
+
 preprocess = function(df){
-    df = merge(df, sessions, by.x="id", by.y="user_id")
-    df$month_created = month(df$date_account_created)
-    df$year_created = year(df$date_account_created)
-    df$year_booked = year(df$date_first_booking)
-    df$month_booked = month(df$date_first_booking)
-    df$diff_booked = as.Date(df$date_first_booking) - as.Date(df$date_account_created)
+    #Normalizes some of the ages
+    df$age[df$age > 150 & !is.na(df$age)] = 2015 - df$age[df$age > 150 & !is.na(df$age)]
+    df$age[df$age > 115 & !is.na(df$age)] = 115
+    df$age[df$age < 18 & !is.na(df$age)] = 18
+    
+        
     return(df)
 }
+
 
 train = preprocess(train)
 test = preprocess(test)
@@ -34,4 +55,4 @@ test = preprocess(test)
 
 
 #Creates submission
-
+write.csv(submission, "prediction.csv", row.names=FALSE)
