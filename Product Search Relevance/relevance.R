8267415c1bd71341e69e0a088316b6b0b8f6e117
Dan <dchen12812@gmail.com>
2016-01-22 14:59:28 -0800

    merged dataset with descriptions

--- Product Search Relevance/relevance.R
+++ Product Search Relevance/relevance.R
@@ -2,6 +2,11 @@
 
 #Read input
 dir = 'input/'
-train = read.csv(paste0(dir,"train_users_2.csv"))
-test = read.csv(paste0(dir,"test_users.csv"))
+train = read.csv(paste0(dir,"train.csv"))
+test = read.csv(paste0(dir,"test.csv"))
+#attributes = read.csv(paste0(dir, "attributes.csv")) #seems to have snippets of descriptions
+descriptions = read.csv(paste0(dir, "product_descriptions.csv"))
 
+#Merged products with their descriptions
+mTrain = merge(train, descriptions)
+mTest = merge(test, descriptions)
