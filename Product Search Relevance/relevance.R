b39d674d51c616731b7c8836a92e0bdc40c2ef20
dchen71 <dchen12812@gmail.com>
2016-01-25 17:18:36 -0800

    glm based on benchmark

--- Product Search Relevance/relevance.R
+++ Product Search Relevance/relevance.R
@@ -28,14 +28,24 @@ word_match = function(words, title, desc){
   return(c(n_title,nwords,n_desc))
 }
 
-train_words <- as.data.frame(t(mapply(word_match,train$search_term,train$product_title,train$product_description)))
-train$nmatch_title <- train_words[,1]
-train$nwords <- train_words[,2]
-train$nmatch_desc <- train_words[,3]
+#Process train
+train_words = as.data.frame(t(mapply(word_match,train$search_term,train$product_title,train$product_description)))
+train$nmatch_title = train_words[,1]
+train$nwords = train_words[,2]
+train$nmatch_desc = train_words[,3]
 
-test_words <- as.data.frame(t(mapply(word_match,test$search_term,test$product_title,test$product_description)))
-test$nmatch_title <- test_words[,1]
-test$nwords <- test_words[,2]
-test$nmatch_desc <- test_words[,3]
+#Process test
+test_words = as.data.frame(t(mapply(word_match,test$search_term,test$product_title,test$product_description)))
+test$nmatch_title = test_words[,1]
+test$nwords = test_words[,2]
+test$nmatch_desc = test_words[,3]
 
 rm(train_words,test_words)
+
+glm_model = glm(relevance~nmatch_title+nmatch_desc+nwords,data=train)
+test_relevance = predict(glm_model,test)
+test_relevance = ifelse(test_relevance>3,3,test_relevance)
+test_relevance = ifelse(test_relevance<1,1,test_relevance)
+
+submission = data.frame(id=test$id, relevance=test_relevance)
+write_csv(submission,"benchmark_submission.csv")
