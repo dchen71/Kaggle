e9d7f465717ef0f77803e65e7158673b137df071
dchen71 <dchen12812@gmail.com>
2016-06-10 16:06:04 -0700

    merged authors with paper authors

--- NIPS 2015 Papers/exploratory.R
+++ NIPS 2015 Papers/exploratory.R
@@ -12,16 +12,37 @@ papers = read_csv("input/Papers.csv")
 #Look at type of events at NIPS
 table(papers$EventType)
 
-#Check last names
-authorNames = strsplit(authors$Name, " ")
-
 #Function to find last name for each row
 findLastName = function(fullname){
   return(fullname[length(fullname)])
 }
+
+##
+## Check last names for authors
+##
+
+#Check last names
+authorNames = strsplit(authors$Name, " ")
+
 lastNames = lapply(authorNames, findLastName)
 lastNames = as.character(lastNames)
 
 #Count number of each last name
 lastNamesCount = as.data.frame(table(lastNames))
 lastNamesCount = lastNamesCount[order(lastNamesCount$Freq),]
+
+##
+## Check last name counts for authors linked to papers
+##
+
+#Find who wrote papers
+publishers = merge(authors, paperAuthors, by.x = "Id", by.y="AuthorId")
+
+#Check last names
+authorNames = strsplit(publishers$Name, " ")
+lastNamesPub = lapply(authorNames, findLastName)
+lastNamesPub = as.character(lastNamesPub)
+
+#Count number of each last name
+lastNamesPubCount = as.data.frame(table(lastNamesPub))
+lastNamesPubCount = lastNamesPubCount[order(lastNamesPubCount$Freq),]
