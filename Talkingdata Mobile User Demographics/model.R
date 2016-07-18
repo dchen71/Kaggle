d81d8484cbd3e8a4bbbd269b99beeeb69c03ad14
Dan <dchen12812@gmail.com>
2016-07-18 00:26:49 -0700

    merge df together

--- Talkingdata Mobile User Demographics/model.R
+++ Talkingdata Mobile User Demographics/model.R
@@ -2,21 +2,41 @@
 ## model
 
 # Packages
-library(readr)
 library(dplyr)
 
 # Read input data
-app_events = read_csv("input/app_events.csv")
-app_labels = read_csv("input/app_labels.csv")
-events = read_csv("input/events.csv")
-labels = read_csv("input/label_categories.csv")
-phone_brand_model = read_csv("input/phone_brand_device_model.csv")
-test = read_csv("input/gender_age_test.csv")
-train = read_csv("input/gender_age_train.csv")
+app_events = read.csv("input/app_events.csv", colClasses = "character")
+app_labels = read.csv("input/app_labels.csv", colClasses = "character")
+events = read.csv("input/events.csv", colClasses = "character")
+labels = read.csv("input/label_categories.csv", colClasses = "character")
+phone_brand_model = read.csv("input/phone_brand_device_model.csv", colClasses = "character")
+test = read.csv("input/gender_age_test.csv", colClasses = "character")
+train = read.csv("input/gender_age_train.csv", colClasses = "character")
+
+sample = read_csv('input/sample_submission.csv')
+
+#Remove columsn that won't be used(memory considerations)
+events$timestamp = NULL
+events$longitude = NULL
+events$latitude = NULL
+
+#Merge data with app_events/events
+events = merge(events, app_events, by = "event_id", all.x = T)
+rm(app_events)
 
 #Merge data with the phone brand and model
-merge(train,phone_brand_model)
-merge(test,phone_brand_model, by=intersect(names(test), names(phone_brand_model)), all.x=FALSE)
+train = merge(train,phone_brand_model, by=intersect(names(train), names(phone_brand_model)), all.x=TRUE)
+test = merge(test,phone_brand_model, by=intersect(names(test), names(phone_brand_model)), all.x=TRUE)
+rm(phone_brand_model)
+
+#Merge app labels with cateogry
+app_labels = merge(app_labels, labels, by = "label_id", all.x = T)
+rm(labels)
+
+#Merge everythig with train and test
+events = merge(events, app_labels, by= "app_id", all.x = T)
+train = merge(train, events, by = "device_id", all.x = T)
+test = merge(test, events, by = "device_id", all.x = T)
 
 #Create submission
 submission = data.frame(test$device_id)
