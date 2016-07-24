1bc7a693f2990705c1124ebc846030e237447a56
Dan <dchen12812@gmail.com>
2016-07-24 00:17:36 -0700

    changed merging

--- Talkingdata Mobile User Demographics/model.R
+++ Talkingdata Mobile User Demographics/model.R
@@ -1,43 +1,72 @@
 ## Kaggle - TalkingData Mobile User Demographics
-## model
+## Basic xgboost model
+### Can potentially implement around app events to find user apps and category for further categorization
+
+## Initialization
 
 # Packages
-library(dplyr)
+#library(dplyr)
+library(xgboost)
+library(Matrix)
 
 # Read input data
-app_events = read.csv("input/app_events.csv", colClasses = "character", stringsAsFactors = TRUE)
-app_labels = read.csv("input/app_labels.csv", colClasses = "character", stringsAsFactors = TRUE)
-events = read.csv("input/events.csv", colClasses = "character", stringsAsFactors = TRUE)
-labels = read.csv("input/label_categories.csv", colClasses = "character", stringsAsFactors = TRUE)
-phone_brand_model = read.csv("input/phone_brand_device_model.csv", colClasses = "character", stringsAsFactors = TRUE)
-test = read.csv("input/gender_age_test.csv", colClasses = "character", stringsAsFactors = TRUE)
-train = read.csv("input/gender_age_train.csv", colClasses = "character", stringsAsFactors = TRUE)
+#app_events = read.csv("input/app_events.csv", colClasses = "character", stringsAsFactors = TRUE)
+#app_labels = read.csv("input/app_labels.csv", colClasses = "character", stringsAsFactors = TRUE)
+#events = read.csv("input/events.csv", colClasses = "character", stringsAsFactors = TRUE)
+#label_cat = read.csv("input/label_categories.csv", colClasses = "character", stringsAsFactors = TRUE)
+phone_brand_model = read.csv("input/phone_brand_device_model.csv", stringsAsFactors = TRUE)
+test = read.csv("input/gender_age_test.csv", stringsAsFactors = TRUE)
+train = read.csv("input/gender_age_train.csv", stringsAsFactors = TRUE)
+
+
+## Cleaning and merging
 
 #Rename the dashes and pluses as . for submission
 train$group = gsub("[^a-zA-Z0-9_]",".", train$group)
 
-#Remove columsn that won't be used(memory considerations)
-events$timestamp = NULL
-events$longitude = NULL
-events$latitude = NULL
+#Remove columns that won't be used(memory considerations)
+##Can potentially use this column to find lenght of time a app is installed/active/etc
+#events$timestamp = NULL
+#events$longitude = NULL
+#events$latitude = NULL
+
+#Merge app labels with cateogry
+##Consider cleaning up data/merging ex. games, fiance, simple/simple 1
+#app_labels = left_join(app_labels, label_cat, by = "label_id")
+#rm(label_cat)
+
+#Merge app labels with app events
+#app_data = left_join(app_labels, app_events, by = "app_id")
+
+#table together category installed/active per user
+
 
 #Merge data with app_events/events
-events = merge(events, app_events, by = "event_id", all.x = T)
-rm(app_events)
+#events = left_join(events, app_events, by = "event_id")
+#events = events[order(events$event_id),]
+#rm(app_events)
 
 #Merge data with the phone brand and model
 train = merge(train,phone_brand_model, by=intersect(names(train), names(phone_brand_model)), all.x=TRUE)
 test = merge(test,phone_brand_model, by=intersect(names(test), names(phone_brand_model)), all.x=TRUE)
 rm(phone_brand_model)
 
-#Merge app labels with cateogry
-app_labels = merge(app_labels, labels, by = "label_id", all.x = T)
-rm(labels)
+
+
+#Merge app labels with events
+#events = merge(events, app_labels,by = "app_id", all.x = T)
 
 #Merge everythig with train and test
-events = merge(events, app_labels, by= "app_id", all.x = T)
-train = merge(train, events, by = "device_id", all.x = T)
-test = merge(test, events, by = "device_id", all.x = T)
+#events = merge(events, app_labels, by= "app_id", all.x = T)
+#train = merge(train, events, by = "device_id", all.x = T)
+#test = merge(test, events, by = "device_id", all.x = T)
+
+
+## Modeling
+#Create model using xgboost
+train_labels = as.numeric(train$group) - 1
+xgbMat = xgb.DMatrix(Matrix(data.matrix(train[,!colnames(train) %in% c("group")])), 
+                     label=train_labels)
 
 #Create submission
 submission = data.frame(test$device_id)
